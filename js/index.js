e3415885192d69cf904d5be6f8ea9d4703ba27ac
PiotrusWatson <piotruswatson@gmail.com>
2021-07-23 15:34:40 +0100

    did some attempts at rolling own dropdown, realised could just cdn mentions???

--- js/index.js
+++ js/index.js
@@ -1,15 +1,34 @@
+var isOut = false;
+
+
+mention = {
+    mentionDenotationChars: ["@"]
+}
 var bindings = {
     tab: {
         key: 9,
         handler: function() {
-            var selection = this.quill.getSelection();
-            var pixelPoint = this.quill.getBounds(selection.index)
-            
+            if (!isOut){
+                var selection = this.quill.getSelection();
+                var pixelPoint = this.quill.getBounds(selection.index);
+                $("#dropdown").css({left: pixelPoint.left, top: pixelPoint.top}).removeClass("invisible");
+                isOut = true;
+            }
+            else{
+                $("#dropdown").addClass("invisible");
+                isOut = false;
+            }
+
         }
     }
 }
 
 var quill = new Quill('#editor', {
-    theme: 'snow'
+    theme: 'snow',
+    modules: {
+        keyboard: {
+            bindings: bindings
+        }
+    }
   });
 
