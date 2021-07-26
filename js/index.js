53342707270c12aca4c8382f0adc8bd37557ec78
PiotrusWatson <piotruswatson@gmail.com>
2021-07-26 16:20:54 +0100

    half fixed it?

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ import "quill-mention";
 import {generateWords} from "./endpoint.js"
 var Quill = require("quill");
 var MAX_PROMPT = 2000;
+var isLoading = false;
 var mention = {
     //assumption: bell won't be generated randomly by a textbot
     mentionDenotationChars: ['\u0007'],
@@ -12,14 +13,19 @@ var mention = {
     spaceAfterInsert:false,
     renderLoading: function(){
         console.log("loading");
+        isLoading = true;
     }
 }
 var bindings = {
     tab: {
         key: 9,
         handler: async function() {
+            if (isLoading){
+                return;
+            }
             var mention = this.quill.getModule('mention')
-                mention.openMenu('\u0007');  
+                mention.openMenu('\u0007');
+                this.quill.deleteText(this.quill.getLength - 2);
         }
     }
 
@@ -55,6 +61,7 @@ function parseMention(content){
 }
 
 $(document).ready(() =>{
+    var index;
     var quill = new Quill('#editor', {
         theme: 'snow',
         modules: {
@@ -67,23 +74,25 @@ $(document).ready(() =>{
     quill.getModule('mention').options.source = 
         async function(searchTerm, renderList, mentionChar) {
         var range = quill.getSelection();
+        index = range.index + range.length;
         var cutoff;
         var text = parseContents(quill.getContents());
         text = sliceText(text, range);
         console.log(text);
-        if (text.charAt(text.length - 1) === "\n"){
-            cutoff = 2
-        }
-        else {
-            cutoff = 1
-        }
-        var slicedText = text.slice(0, text.length - cutoff);
+        var slicedText = text.slice(0, text.length - 1);
         generateWords(slicedText, 5, 1, 0.9)
             .then((res) =>{
                 let values = res.result
+                isLoading = false;
                 renderList(values, searchTerm);
             });
         
+    }
+
+    quill.getModule('mention').options.onSelect = function(item, insertItem){
+        console.log("THERE IS THE ITEM");
+        console.log(item);
+        quill.insertText(index, item.value);
     }
 
 });
