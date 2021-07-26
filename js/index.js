cafc2264acef3bcbb0aeddec67ba7f493fba8bca
PiotrusWatson <piotruswatson@gmail.com>
2021-07-26 11:38:46 +0100

    made the basic shit work

--- js/index.js
+++ js/index.js
@@ -3,27 +3,15 @@ import "regenerator-runtime/runtime";
 import "quill-mention";
 import {generateWords} from "./endpoint.js"
 var Quill = require("quill");
-
-var cool_values = [
-    {id: 1, value: "hi"},
-    {id: 2, value: "sup"}
-]
-var isOpen = false;
+var MAX_PROMPT = 2000;
 var mention = {
     //assumption: bell won't be generated randomly by a textbot
     mentionDenotationChars: ['\u0007'],
     allowedChars: /^[\t]*$/,
     showDenotionChar: false,
+    spaceAfterInsert:false,
     renderLoading: function(){
         console.log("loading");
-    },
-    source: async function(searchTerm, renderList, mentionChar) {
-        generateWords("hey gamers", 10, 1, 0.9)
-            .then((res) =>{
-                let values = res.result
-                renderList(values, searchTerm);
-            });
-        
     }
 }
 var bindings = {
@@ -32,12 +20,38 @@ var bindings = {
         handler: async function() {
             var mention = this.quill.getModule('mention')
                 mention.openMenu('\u0007');  
-                isOpen = true;  
-
         }
     }
 
 
+}
+
+function getText(quill, range){
+    var endPoint = range.index + range.length;
+    var startPoint;
+    if (endPoint - MAX_PROMPT < 0){
+        startPoint = 0;
+    } else {
+        startPoint = endPoint - MAX_PROMPT;
+    }
+    return quill.getContents(startPoint);
+}
+
+function parseContents(contents){
+    console.log(contents);
+    var text = ""
+    for (var item of contents.ops){
+        text += parseMention(item.insert);
+    }
+    return text;
+}
+
+function parseMention(content){
+    if (typeof content === "string"){
+        return content;
+    }
+    return content.mention.value;
+    
 }
 
 $(document).ready(() =>{
@@ -50,6 +64,26 @@ $(document).ready(() =>{
             }
         }
       });
+    quill.getModule('mention').options.source = 
+        async function(searchTerm, renderList, mentionChar) {
+        var range = quill.getSelection();
+        var cutoff;
+        var text = parseContents(getText(quill, range));
+        console.log(text);
+        if (text.charAt(text.length - 1) === "\n"){
+            cutoff = 2
+        }
+        else {
+            cutoff = 1
+        }
+        var slicedText = text.slice(0, text.length - cutoff);
+        generateWords(slicedText, 10, 1, 0.9)
+            .then((res) =>{
+                let values = res.result
+                renderList(values, searchTerm);
+            });
+        
+    }
 
 });
 
